4e18ab9c143002d34882ff24b7e73ca7ae702b0c
kiranbum1991 <tomlom1984@hotmail.co.uk>
2019-07-18 09:48:01 +0100

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,11 +3,11 @@ def create_an_empty_array
 end
 
 def create_an_array
-  [1, 2, 3, 4]
+  ["tom", "can", "do it", "GO!"]
 end
 
 def add_element_to_end_of_array(array, element)
-  array.push("arrays!")
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
@@ -27,7 +27,7 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  array[0]
+ array[0]
 end
 
 def retrieve_last_element_from_array(array)
@@ -35,5 +35,5 @@ def retrieve_last_element_from_array(array)
 end
 
 def update_element_from_index(array, index_number, element)
-  array[4]="totally"
+  array[0] = "totally"
 end
